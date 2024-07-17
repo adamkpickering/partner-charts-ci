585636ba3763b71f82921b3480bc0c6cae94bc26
Adam Pickering <adam.pickering@suse.com>
2024-07-17 14:50:03 -0600

    Introduce paths.Paths type

--- pkg/paths/paths.go
+++ pkg/paths/paths.go
@@ -2,9 +2,36 @@ package paths
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/sirupsen/logrus"
 )
+
+var paths *Paths
+
+// Paths is a type that contains all of the paths that are relevant
+// to this tool. This approach facilitates unit testing.
+type Paths struct {
+	Assets            string
+	Charts            string
+	Packages          string
+	ConfigurationYaml string
+	IndexYaml         string
+}
+
+func Get() Paths {
+	if paths == nil {
+		repoRoot := GetRepoRoot()
+		paths = &Paths{
+			Assets:            filepath.Join(repoRoot, "assets"),
+			Charts:            filepath.Join(repoRoot, "charts"),
+			Packages:          filepath.Join(repoRoot, "packages"),
+			ConfigurationYaml: filepath.Join(repoRoot, "configuration.yaml"),
+			IndexYaml:         filepath.Join(repoRoot, "index.yaml"),
+		}
+	}
+	return *paths
+}
 
 // Fetches absolute repository root path
 func GetRepoRoot() string {
