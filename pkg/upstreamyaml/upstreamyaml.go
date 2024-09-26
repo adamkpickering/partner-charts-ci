1152194457eb2e9fce8a7df27e95eb21d8f1331a
Adam Pickering <adam.pickering@suse.com>
2024-09-26 14:11:41 -0600

    Save progress

--- pkg/upstreamyaml/upstreamyaml.go
+++ pkg/upstreamyaml/upstreamyaml.go
@@ -48,6 +48,26 @@ func (upstreamYaml *UpstreamYaml) SetDefaults() {
 	}
 }
 
+func (upstreamYaml *UpstreamYaml) Validate() error {
+	// if Fetch != "latest", HelmChart and HelmRepo must be present
+	// if TrackVersions is present, HelmChart and HelmRepo must be present
+
+	// if ArtifactHubPackage is present, ArtifactHubRepo must be present
+	// if ArtifactHubRepo is present, ArtifactHubPackage must be present
+
+	// if GitBranch is present, GitRepo must be present
+	// if GitHubRelease is present, GitRepo must be present
+	// if GitSubDirectory is present, GitRepo must be present
+
+	// if HelmChart is present, HelmRepo must be present
+	// if HelmRepo is present, HelmChart must be present
+
+	// one of ArtifactHubPackage and ArtifactHubRepo, GitRepo (et al) or
+	// HelmRepo and HelmChart must be present
+
+	return nil
+}
+
 func Parse(upstreamYamlPath string) (*UpstreamYaml, error) {
 	logrus.Debugf("Attempting to parse %s", upstreamYamlPath)
 	contents, err := os.ReadFile(upstreamYamlPath)
@@ -62,5 +82,9 @@ func Parse(upstreamYamlPath string) (*UpstreamYaml, error) {
 
 	upstreamYaml.SetDefaults()
 
-	return upstreamYaml, err
+	if err := upstreamYaml.Validate(); err != nil {
+		return nil, fmt.Errorf("invalid upstream.yaml: %w", err)
+	}
+
+	return upstreamYaml, nil
 }
